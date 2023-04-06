7fca2cdc5b9ba06ea2d1b0936513110ed49fb48a
Alex Cheng <alexcheng@college.harvard.edu>
2023-04-06 00:35:50 -0400

    add database for chat messages

--- chat/server.py
+++ chat/server.py
@@ -1,60 +1,60 @@
+import argparse
 import logging
 import socket
 import sys
-from _thread import *
-from concurrent import futures
+import sqlite3
+from threading import Event, Thread
 
-from utils import get_server_config_from_file
-from wire.server import client_thread
 from wire.chat_service import Chat
+from wire.server import client_thread
 
 # global variables and configurations
 YAML_CONFIG_PATH = '../config.yaml'
-IP_ADDRESS, PORT = get_server_config_from_file(YAML_CONFIG_PATH)
+IP_ADDRESS = 'localhost'
+PORTS = [5555, 6666, 7777]
 logging.basicConfig(format='[%(asctime)-15s]: %(message)s', level=logging.INFO)
 
 
-def main():
-    # Check if enough arguments are passed
-    if len(sys.argv) != 2:
-        print('Error: Incorrect Usage\n\
-              Correct usage: server.py [implementation]')
-        sys.exit()
+def run_server(server_number):
+    # Setting up the server
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((IP_ADDRESS, PORTS[server_number]))
+    server.listen(5)
 
-    # Wire protocol implementation of the client
-    if sys.argv[1] == 'wire':
-        # Setting up the server
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server.bind((IP_ADDRESS, PORT))
-        server.listen(5)
+    # Create a Chat object to handle all the chat logic
+    logging.info('Starting Wire Protocol Server')
+    chat_app = Chat()
 
-        # Create a Chat object to handle all the chat logic
-        logging.info('Starting Wire Protocol Server')
-        chat_app = Chat()
+    # read the id column from latest entry by timestamp in the database
+    sql_conn = sqlite3.connect('./logs/chat.db')
+    c = sql_conn.cursor()
+    id = c.execute("SELECT id FROM messages ORDER BY timestamp DESC LIMIT 1")
 
-        while True:
-            try:
-                # Listen for and establish connection with incoming clients
-                conn, addr = server.accept()
+    while True:
+        try:
+            # Listen for and establish connection with incoming clients
+            conn, addr = server.accept()
 
-                # prints the address of the user that just connected
-                logging.info(addr[0] + " connected.")
+            # prints the address of the user that just connected
+            logging.info(addr[0] + " connected.")
+            # creates a new thread for incoming client
+            start_new_thread(client_thread, (chat_app, conn, addr, id))
+        except KeyboardInterrupt:
+            logging.info('Stopping Server.')
+            break
 
-                # creates a new thread for incoming client
-                start_new_thread(client_thread, (chat_app, conn, addr))
-            except KeyboardInterrupt:
-                logging.info('Stopping Server.')
-                break
-
-        # Close the server socket
-        conn.close()
-        server.close()
-    else:
-        print('Error: Incorrect Usage\n\
-              Correct usage: Implementation must be either "wire" or "grpc"')
+    # Close the server socket
+    conn.close()
+    server.close()
     
     sys.exit()
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Replicated Chat Server")
+
+    parser.add_argument("--server_number", "-s", type=int)
+
+    args = parser.parse_args()
+    
+    run_server(args.server_number)
--- chat/wire/client.py
+++ chat/wire/client.py
@@ -1,8 +1,8 @@
 
 from threading import *
+import sqlite3
 
 from wire.wire_protocol import unpack_packet
-
 
 class ReceiveMessages(Thread):
     def __init__(self, server):
@@ -10,10 +10,19 @@ class ReceiveMessages(Thread):
         self.__server = server
 
     def run(self):
+        sql_conn = sqlite3.connect('../logs/chat.db')
+        c = sql_conn.cursor()
         while True:
             try:
                 message = self.__server.recv(1024)
                 _, data = unpack_packet(message)
+                # get all values in the data column of the database
+                messages = c.execute("SELECT data FROM messages")
+                # check if data in messages
+                if data in messages:
+                    # delete the earliest row based on timestamp where the message is from the database
+                    c.execute("DELETE FROM messages WHERE data = ? ORDER BY timestamp ASC LIMIT 1", (data,))
+                    break
                 print(data)
             except:
                 break
